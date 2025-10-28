b8e98a8ecd49990aead9572e3ca9cfe84d02d3f2
SaiPrasanna1799 <saiprasannamandapalli1799@gmail.com>
2025-10-28 23:11:15 +0530

    studentdetails

--- src/Components/StudentDetails.js
+++ src/Components/StudentDetails.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import { studentsData } from "./studentsData";
+import { useNavigate } from "react-router-dom";
 
 const StudentDetails = () => {
+  const {id} = useParams();
+  const student = studentsData.find((s) => (s.id ===parseInt(id)))
+  const navigate = useNavigate();
   return (
-    <div>StudentDetails</div>
+    <div className="bg-red-300">
+      <p>Name:{student.name}</p>
+      <p>Id:{student.id}</p>
+      <p>course:{student.course}</p>
+      <p>Grade:{student.grade}</p>
+      <button onClick={() => navigate(-1)}>Back</button>
+    </div>
   )
-}
-
+};
 export default StudentDetails;
--- src/Components/StudentList.js
+++ src/Components/StudentList.js
@@ -1,12 +1,21 @@
 import { studentsData } from "./studentsData";
+import { Link } from "react-router-dom";
 
 const StudentList = () => {
   return (
-    <div>
-      <h1>All Students</h1>
-      {studentsData.map((student) => <p>{student.name}</p>)}
+    <div className="bg-red-300 h-screen">
+      <h1 className="text-center font-bold">All Students</h1>
+      {studentsData.map((student) => 
+      <Link key={student.id} to={`/students/${student.id}`}>
+      <div key={student.id}>
+      {/* <p className="font-bold">{student.id}</p> */}
+      <p className="font-bold">{student.name}</p>
+      {/* <p className="font-bold">{student.course}</p>
+      <p className="font-bold">{student.grade}</p> */}
+      </div>
+      </Link>
+      )}
     </div>
-  )
-}
-
+  );
+};
 export default StudentList;
